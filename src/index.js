0e276d0d508dca3acdd3faab2986d1cdea85b346
Arles96 <arles.cerrato@unitec.edu>
2018-06-11 13:27:51 -0600

    Se agrego la configuracion de firebase

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import firebase from 'firebase'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAEafqSyA6sqVuUAfsOMO36yQFbZM-duhE",
+    authDomain: "b4us-e9e2a.firebaseapp.com",
+    databaseURL: "https://b4us-e9e2a.firebaseio.com",
+    projectId: "b4us-e9e2a",
+    storageBucket: "b4us-e9e2a.appspot.com",
+    messagingSenderId: "229628115831"
+};
+firebase.initializeApp(config)
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
