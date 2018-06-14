96f3197e217647762785d446ff93e0bcfcac973e
Arles96 <arles.cerrato@unitec.edu>
2018-06-13 21:20:59 -0600

    Creacion del archivo Pricing

--- src/BigComponent/Principal.js
+++ src/BigComponent/Principal.js
@@ -3,12 +3,25 @@ import AboutUs from '../components/AboutUS/AboutUs'
 import Blog from '../components/Blog/Blog'
 import Navbar from '../components/Navbar/Navbar'
 import GetStarted from '../components/GetStarted/GetStarted'
+import Greeting from '../components/Greeting/Greeting'
 
 export default class Principal extends Component {
 
     constructor(){
         super()
+        this.state = {
+            counter : 0
+        }
     }
+
+    handleContent(){
+        if (this.state.counter===0){
+            return (
+                <Greeting />
+            )
+        }
+    }
+
 
     render(){
         return (
