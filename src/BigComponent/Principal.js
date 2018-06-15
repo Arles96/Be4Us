7fa1533fcc172520c4ed419ae28acc3a4b59d240
CJAlvarez <cristhianalvarez96@gmail.com>
2018-06-15 14:36:49 -0600

    CJ - Backup (#6) -> Create Full Logo, Make Responsive Greeting

--- src/BigComponent/Principal.js
+++ src/BigComponent/Principal.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react'
-import AboutUs from '../components/AboutUS/AboutUs'
-import Blog from '../components/Blog/Blog'
+// import AboutUs from '../components/AboutUS/AboutUs'
+// import Blog from '../components/Blog/Blog'
 import Navbar from '../components/Navbar/Navbar'
-import GetStarted from '../components/GetStarted/GetStarted'
+// import GetStarted from '../components/GetStarted/GetStarted'
 import Greeting from '../components/Greeting/Greeting'
-import Pricing from '../components/Pricing/Pricing'
-import Developers from '../components/Developers/Developers'
+// import Pricing from '../components/Pricing/Pricing'
+// import Developers from '../components/Developers/Developers'
 
 export default class Principal extends Component {
 
@@ -28,13 +28,7 @@ export default class Principal extends Component {
     render(){
         return (
             <div>
-                <Navbar />
                 <Greeting />
-                <AboutUs />
-                <GetStarted />
-                <Blog/>
-                <Pricing />
-                <Developers/>
             </div>
         )
     }
--- src/components/Greeting/Greeting.js
+++ src/components/Greeting/Greeting.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './Greeting.css';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
+//import Button from '@material-ui/core/Button';
 import ResizeDetector from 'react-resize-detector';
 
 class Greeting extends Component {
@@ -9,48 +8,88 @@ class Greeting extends Component {
     constructor() {
         super();
         this.state = {
-            width: undefined,
-            height: undefined,
+            width: document.innerWidth,
+            height: document.innerHeight,
             sizeFormat: 0
         };
         this.onResize = (width, height) => this.setState({ width, height });
         this.sizeChange = this.sizeChange.bind(this);
+        this.setSizeChange = this.setSizeChange.bind(this);
+        this.draw = this.draw.bind(this);
+    }
+
+    setSizeChange() {
+        this.setState({
+            width: document.innerWidth,
+            height: document.innerHeight
+        });
+        this.sizeChange();
     }
 
     sizeChange() {
-        /*45 vw + 60vh*/
         //console.log(this.state.width + ", " + this.state.height);
-        if ((this.state.height * 0.8) >= this.state.width && this.state.sizeFormat == 0) {
-            this.changeSizeFormat();
+        if ((this.state.height * 0.75) >= this.state.width && this.state.sizeFormat === 0) {
+            this.changeSizeFormat(true);
+        } else {
+            this.changeSizeFormat(false);
         }
     }
 
-    changeSizeFormat() {
-        document.getElementsByClassName("Be_").setAttribute("style", "font-size: 25vw !important;")
-        document.getElementsByClassName("Us_").setAttribute("style", "font-size: 25vw !important;")
-        document.getElementsByClassName("For_").setAttribute("style", "font-size: 45vw !important;")
+    draw() {
+        this.canvas_dark = document.getElementsByClassName("Canvas_Dark")[0];
+        this.canvas_soft = document.getElementsByClassName("Canvas_Soft")[0];
+        if ((this.canvas_dark + "") !== "undefined" && (this.canvas_soft + "") !== "undefined") {
+            if (this.state.height <= this.state.width) {
+                this.canvas_dark.width = this.state.height / 2;
+                this.canvas_dark.height = this.state.height / 2;
+                this.canvas_soft.width = this.state.height / 2;
+                this.canvas_soft.height = this.state.height / 2;
+            } else {
+                this.canvas_dark.width = this.state.width / 2;
+                this.canvas_dark.height = this.state.width / 2;
+                this.canvas_soft.width = this.state.width / 2;
+                this.canvas_soft.height = this.state.width / 2;
+            }
+        }
     }
 
+    changeSizeFormat(k) {
+        console.log(this.state.width + ", " + this.state.height);
+        if (k === true) {
+            document.getElementsByClassName("Be_")[0].setAttribute("style", "margin-top: 20vw;")
+            document.getElementsByClassName("Be_")[0].setAttribute("style", "font-size: 25vw !important; height: 18vw;")
+            document.getElementsByClassName("Us_")[0].setAttribute("style", "font-size: 25vw !important; height: 18vw;")
+            document.getElementsByClassName("For_")[0].setAttribute("style", "font-size: 70vw !important; height: 18vw; line-height : 50vw !important;")
+            document.getElementsByClassName("text-greet")[0].setAttribute("style", "font-size: 7vw !important; margin: 10px;")
+        } else {
+            document.getElementsByClassName("Be_")[0].setAttribute("style", "margin-top: 10vh;")
+            document.getElementsByClassName("Be_")[0].setAttribute("style", "font-size: 25vh !important; height: 18vh;")
+            document.getElementsByClassName("Us_")[0].setAttribute("style", "font-size: 25vh !important; height: 18vh;")
+            document.getElementsByClassName("For_")[0].setAttribute("style", "font-size: 60vh !important; height: 18vh; line-height : 45vh !important;")
+            document.getElementsByClassName("text-greet")[0].setAttribute("style", "font-size: 4vh !important; margin: 10px;")
+        }
+    }
+
+
     render() {
-        const { width, height } = this.state;
         return (
             <div className="greeting">
                 <div className="contain-greet" >
-
                     {/* Contenedor del Logo General*/}
-                    <div className="grid-container" >
-                        <div>
-                            <p className="Logo_ Be_">Be</p>
-                            <p className="Logo_ Us_">Us</p>
-                        </div>
-                        <div >
-                            <p className="Logo_ For_">4</p>
-                        </div>
+                    <div className="grid-container">
+                        <canvas className="Canvas_Soft"></canvas>
+                        <canvas className="Canvas_Dark"></canvas>
+                        <p className="Logo_ Be_">Be</p>
+                        <p className="Logo_ For_">4</p>
+                        <p className="Logo_ Us_">Us</p>
                     </div>
                     <p className="text-greet" >Descubre una manera más fácil para organizar y planificar tus eventos, colectivamente y en tiempo real.</p>
                 </div>
                 <ResizeDetector handleWidth handleHeight onResize={this.onResize} />
                 <ResizeDetector handleWidth handleHeight onResize={this.sizeChange} />
+                <ResizeDetector handleWidth handleHeight onResize={this.draw} />
+                {window.resizeTo(window.innerWidth, window.innerHeight)}
+                {this.draw()}
             </div>
         );
     }
