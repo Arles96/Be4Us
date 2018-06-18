3ba14746f39fd99e34acac4725a9663f07fb7a50
CJAlvarez <cristhianalvarez96@gmail.com>
2018-06-18 11:10:03 -0600

    CJ - Backup (#12) -> Prevent

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Principal from './BigComponent/Principal'
+import Error_404 from './BigComponent/404'
 
 export default () =>(
     <BrowserRouter>
         <Switch>
             <Route path="/" exact component={Principal} />
+            <Route path="/" exact component={Error_404} />
         </Switch>
     </BrowserRouter>
 )
