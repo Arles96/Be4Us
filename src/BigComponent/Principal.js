391e87d1cba63b2cac044620bc5d631eae8cf635
Arles96 <arles.cerrato@unitec.edu>
2018-06-21 10:14:17 -0600

    Correccion de about us

--- src/BigComponent/Principal.js
+++ src/BigComponent/Principal.js
@@ -14,52 +14,18 @@ export default class Principal extends Component {
         this.state = {
             counter: 0
         }
-        this.handleScroll = this.handleScroll.bind(this)
     }
-
-    handleContent(){
-        if (this.state.counter===0){
-            return <div className="animated fadeInUp star" ><Greeting /></div>
-        }
-        else if (this.state.counter===1){
-            return <div className="animated fadeInUp star" ><GetStarted /></div>
-        }
-        else if (this.state.counter===2){
-            return <div className="animated fadeInUp star" ><Developers /></div>
-        }
-        else if (this.state.counter===3){
-            return <div className="animated fadeInUp star" ><Blog /></div>
-        }
-        else if (this.state.counter===4){
-            return <div className="animated fadeInUp star" ><AboutUs /></div>
-        }
-        else {
-            return <div className="animated fadeInUp star" ><Pricing /></div>
-        }
-    }
-
-    handleScroll(e){
-        if (Math.sign(e.deltaY)===-1){
-            if (this.state.counter>0){
-                this.setState({
-                    counter : this.state.counter - 1
-                })
-            }
-        }else {
-            if (this.state.counter<5){
-                this.setState({
-                    counter : this.state.counter + 1
-                })
-            }
-        }
-    }
-
 
     render() {
         return (
-            <div onWheel={this.handleScroll} >
+            <div>
                 <Navbar />
-                {this.handleContent()}
+                <Greeting />
+                <GetStarted />
+                <Developers />
+                <Blog />
+                <AboutUs />
+                <Pricing />
             </div>
         )
     }
