601dad698b076f601491ff40fc462325000477fe
Arles96 <arles.cerrato@unitec.edu>
2018-06-30 18:04:27 -0600

    Nombrando las funciones para capturar la data

---
+++ src/data/user.js
@@ -0,0 +1,23 @@
+import {auth, database, storage} from './firebase'
+
+const url = 'http://localhost:4000'
+
+export const getUser = () => {
+    fetch()
+}
+
+export const signup = () => {
+    fetch()
+}
+
+export const signin = () => {
+    fetch()
+}
+
+export const uploadImage = () => {
+    fetch()
+}
+
+export const removeImage = () => {
+    fetch()
+}
